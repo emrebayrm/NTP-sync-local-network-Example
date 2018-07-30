5a7219fb926abdcbdb759c2afe43b1d8a84ba201
Emre Bayram <e.bayram@gtu.edu.tr>
2018-07-30 07:01:25 -0400

    v1

--- vip-down.sh
+++ vip-down.sh
@@ -1,2 +1,20 @@
 #! /bin/sh
+timestamp=$(date +%u%m%d%H%M%S)
+mylog=/var/log/clock_sync.log
+echo "[$timestamp] - Going listen mode\n" >> mylog
+#echo "[$timestamp] - \n" >> mylog
 /sbin/ip addr del 10.224.172.252/24 dev enp0s3
+
+echo "[$timestamp] - Config file copying \n" >> mylog
+mv ntp.conf ntp.conf.bckp
+cp ntp.conf.client ntp.conf
+echo "[$timestamp] - Completed \n" >> mylog
+
+echo "[$timestamp] - Service is restarting\n" >> mylog
+service ntp stop
+service ntp start
+echo "[$timestamp] - Service restarted \n" >> mylog
+
+echo "[$timestamp] - Host is searching ...\n" >> mylog
+ntptrace >> /dev/null
+echo "[$timestamp] - Done \n" >> mylog
--- vip-up.sh
+++ vip-up.sh
@@ -1,2 +1,17 @@
 #! /bin/sh
+
+timestamp=$(date +%u%m%d%H%M%S)
+mylog=/var/log/clock_sync.log
+echo "[$timestamp] - Going broadcast mode\n" >> mylog
+#echo "[$timestamp] - \n" >> mylog
 /sbin/ip addr add 10.224.172.252/24 dev enp0s3
+
+echo "[$timestamp] - Config file copying \n" >> mylog
+mv ntp.conf ntp.conf.bckp
+cp ntp.conf.server ntp.conf
+echo "[$timestamp] - Completed \n" >> mylog
+
+echo "[$timestamp] - Service is restarting\n" >> mylog
+service ntp stop
+service ntp start
+echo "[$timestamp] - Service restarted \n" >> mylog
