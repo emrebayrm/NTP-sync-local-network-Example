f2b88bbb17e4afe8737f3d7908b14de47b650424
Emre Bayram <e.bayram@gtu.edu.tr>
2018-08-02 10:28:25 +0300

    update up script

--- vip-up.sh
+++ vip-up.sh
@@ -4,7 +4,11 @@ mylog=/var/log/clock_sync.log
 
 echo "[$timestamp] - Going broadcast mode" >> $mylog
 #echo "[$timestamp] - \n" >> mylog
-ntpdate -u 10.224.172.252
+ntpdate -u 10.224.172.252 >> $mylog
+if [ $? -eq 0]
+	echo "[$timestamp] - Look like master still awake" >> $mylog
+	exit 0
+fi
 /sbin/ip addr add 10.224.172.252/24 dev enp0s3
 
 echo "[$timestamp] - Config file copying" >> $mylog
